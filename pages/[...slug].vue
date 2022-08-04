45abebdc3b66a88f4bd91d7008ec7c4c8313b879
xiaoqiyang <meetqy@qq.com>
2022-08-04 11:13:09 +0800

    优化slug

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -12,114 +12,120 @@
       </div>
     </div>
 
-    <div class="lg:container w-full bg-info/5 pb-8 rounded-b">
-      <article
-        class="md:prose prose-sm prose-p:mb-0 prose-h2:mb-0 !max-w-full px-4 pt-4"
-      >
-        <div
-          class="pl-0 md:pl-4 shadow bg-base-100 overflow-hidden rounded-box mb-4 flex items-center justify-between flex-col md:flex-row"
-          v-if="page.previews"
+    <div class="w-full bg-info/5 pb-8 rounded-b">
+      <div class="lg:container">
+        <article
+          class="md:prose prose-sm prose-p:mb-0 prose-h2:mb-0 !max-w-full px-4 pt-4"
         >
-          <h1 class="!m-0 py-4 md:py-0">{{ page.title }}</h1>
-
           <div
-            class="flex justify-end max-h-24 flex-1 md:flex-none items-center bg-primary md:rounded-r-box rounded-b-box"
+            class="shadow flex items-center justify-between bg-base-100 overflow-hidden rounded-box flex-col lg:flex-row lg:pl-4 pt-4 lg:pt-0"
+            v-if="page.previews"
           >
-            <div
-              class="flex bg-base-300 items-center md:max-w-md w-full overflow-x-scroll"
-            >
-              <div
-                v-for="(item, i) in page.previews"
-                :key="i"
-                @click="activePreviewIndex = i"
-                class="p-1 border-2 border-transparent flex-shrink-0"
-                :class="{
-                  '!border-primary ': activePreviewIndex === i,
-                }"
+            <h1 class="!m-0">{{ page.title }}</h1>
+
+            <div class="w-full lg:w-1/3 flex justify-end mt-4 lg:mt-0">
+              <Swiper
+                :slides-per-view="4"
+                :space-between="12"
+                class="flex-1 bg-base-300"
+                @tap="onTap"
               >
-                <img
-                  class="object-cover w-24 h-24 object-top m-0 p-0"
-                  v-lazy="vLazy(useAsset(item, 'embed,f_webp,s_300x300'))"
-                />
-              </div>
+                <SwiperSlide v-for="(item, i) in page.previews" :key="i">
+                  <img
+                    v-lazy="vLazy(useAsset(item, 'embed,f_webp,s_300x300'))"
+                    class="w-full max-h-24 object-cover object-top !m-1 outline-4 outline"
+                    :class="
+                      activePreviewIndex === i
+                        ? 'outline-primary'
+                        : 'outline-base-100'
+                    "
+                  />
+                </SwiperSlide>
+              </Swiper>
+
+              <NuxtLink
+                :to="`${page._path.replace('introduce', 'template')}`"
+                class="w-12 shrink-0 bg-primary flex justify-center items-center !no-underline"
+              >
+                <span class="material-symbols-outlined text-primary-content">
+                  fullscreen
+                </span>
+              </NuxtLink>
             </div>
-            <NuxtLink
-              :to="`${page._path.replace('introduce', 'template')}`"
-              class="px-4 !h-full bg-primary !text-primary-content flex justify-center items-center !no-underline"
-            >
-              <span class="material-symbols-outlined"> fullscreen </span>
-            </NuxtLink>
           </div>
-        </div>
 
-        <div
-          v-if="page.previews"
-          class="w-full flex-shrink-0 rounded-box bg-neutral max-h-[80vh] overflow-y-auto transition-all"
-          v-for="(item, i) in page.previews"
-          v-show="activePreviewIndex === i"
-        >
-          <img
-            v-lazy="vLazy(useAsset(item, 'f_web'))"
-            class="m-auto !my-0 transition-all object-center"
-            :class="
-              full
-                ? 'object-contain cursor-zoom-out'
-                : 'w-96 object-fill cursor-zoom-in'
-            "
-            @click="full = !full"
-          />
-        </div>
-
-        <p class="mt-8 flex items-center">
-          <span class="material-symbols-outlined"> sell </span>
-          <span class="ml-1 uppercase">tags:</span>
-          <NuxtLink
-            :to="`/tag/${item}`"
-            class="btn btn-primary btn-xs ml-2 !no-underline !text-primary-content"
-            @click="cleanStorage"
-            v-for="item in page.tags"
-          >
-            {{ item }}
-          </NuxtLink>
-        </p>
-
-        <div class="mt-4 flex items-center" v-if="page.source">
-          <span class="material-symbols-outlined"> my_location </span>
-          <span class="ml-1 uppercase">source:</span>
-          《<span class="line-clamp-1">
-            <a
-              :href="page.source"
-              target="_blank"
-              class="text-primary uppercase"
-              >{{ page.source }}</a
-            > </span
-          >》
-        </div>
-
-        <h2 class="capitalize">more</h2>
-        <div
-          class="grid transition-all xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-x-4"
-        >
           <div
-            v-for="item in moreContent"
-            :key="item._id"
-            @click="$router.push(item._path)"
-            class="cursor-zoom-in"
+            v-if="page.previews"
+            class="w-full flex-shrink-0 rounded-box bg-base-200 max-h-[80vh] overflow-y-auto transition-all mt-8 shadow"
+            v-for="(item, i) in page.previews"
+            v-show="activePreviewIndex === i"
           >
             <img
-              v-lazy="vLazy(useAsset(item.previews[0], 'f_webp,s_500x500'))"
-              class="w-full aspect-square object-cover object-top rounded-box border border-base-200 shadow"
+              v-lazy="vLazy(useAsset(item, 'f_web'))"
+              class="m-auto !my-0 transition-all object-center"
+              :class="
+                full
+                  ? 'object-contain cursor-zoom-out'
+                  : 'w-96 object-fill cursor-zoom-in'
+              "
+              @click="full = !full"
             />
-            <p class="px-2">{{ item.title }}</p>
           </div>
-        </div>
-      </article>
+
+          <p class="mt-8 flex items-center">
+            <span class="material-symbols-outlined"> sell </span>
+            <span class="ml-1 uppercase">tags:</span>
+            <NuxtLink
+              :to="`/tag/${item}`"
+              class="btn btn-primary btn-xs ml-2 !no-underline !text-primary-content"
+              @click="cleanStorage"
+              v-for="item in page.tags"
+            >
+              {{ item }}
+            </NuxtLink>
+          </p>
+
+          <div class="mt-4 flex items-center" v-if="page.source">
+            <span class="material-symbols-outlined"> my_location </span>
+            <span class="ml-1 uppercase">source:</span>
+            《<span class="line-clamp-1">
+              <a
+                :href="page.source"
+                target="_blank"
+                class="text-primary uppercase"
+                >{{ page.source }}</a
+              > </span
+            >》
+          </div>
+
+          <h2 class="capitalize">more</h2>
+          <div
+            class="grid transition-all xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-x-4"
+          >
+            <div
+              v-for="item in moreContent"
+              :key="item._id"
+              @click="$router.push(item._path)"
+              class="cursor-zoom-in"
+            >
+              <img
+                v-lazy="vLazy(useAsset(item.previews[0], 'f_webp,s_500x500'))"
+                class="w-full aspect-square object-cover object-top rounded-box border border-base-200 shadow"
+              />
+              <p class="px-2">{{ item.title }}</p>
+            </div>
+          </div>
+        </article>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
 import { useStorage } from "@vueuse/core";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+
 const { page } = useContent();
 
 // console.log(page);
@@ -127,6 +133,11 @@ const { page } = useContent();
 // 当前选中的预览图
 const activePreviewIndex = ref(0);
 const full = ref(false);
+
+const onTap = (swiper, event) => {
+  activePreviewIndex.value = swiper.clickedIndex;
+  swiper.slideTo(swiper.clickedIndex);
+};
 
 const moreContent = await getContentByTag(
   page.value.tags,
