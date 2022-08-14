0934ea8540a4bec888c73c05a40981851e55c7c1
xiaoqiyang <meetqy@qq.com>
2022-08-14 16:27:29 +0800

    feat: sitemap

---
+++ server/routes/sitemap.xml.ts
@@ -0,0 +1,34 @@
+import { SitemapStream, streamToPromise } from "sitemap";
+import { serverQueryContent } from "#content/server";
+
+export default defineEventHandler(async (event) => {
+  // Fetch all documents
+  const docs = await serverQueryContent(event).find();
+  const sitemap = new SitemapStream({
+    hostname: "https://wcao.cc",
+  });
+
+  const allTags: string[] = [];
+
+  // introduce
+  for (const doc of docs) {
+    (doc.tags as string[]).forEach((item) => {
+      if (!allTags.includes(item)) {
+        allTags.push(item);
+        sitemap.write({
+          url: "/tag/" + item,
+          changefreq: "monthly",
+        });
+      }
+    });
+
+    sitemap.write({
+      url: doc._path,
+      changefreq: "monthly",
+    });
+  }
+
+  sitemap.end();
+
+  return streamToPromise(sitemap);
+});
