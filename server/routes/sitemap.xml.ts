d75c00fd74988df1656946105db0d28163e0c031
xiaoqiyang <meetqy@qq.com>
2022-09-08 18:00:24 +0800

    chore: sitemap

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -5,7 +5,7 @@ export default defineEventHandler(async (event) => {
   // Fetch all documents
   const docs = await serverQueryContent(event).find();
   const sitemap = new SitemapStream({
-    hostname: "https://wcao.cc",
+    hostname: "https://css.ist",
   });
 
   const allTags: string[] = [];
