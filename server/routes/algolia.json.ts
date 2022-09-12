0b170731149ad42269afe64135169059e6c9071a
xiaoqiyang <meetqy@qq.com>
2022-09-12 19:52:54 +0800

    chore: algolia.json

--- server/routes/algolia.json.ts
+++ server/routes/algolia.json.ts
@@ -2,5 +2,19 @@ import { serverQueryContent } from "#content/server";
 
 export default defineEventHandler(async (event) => {
   const docs = await serverQueryContent(event).find();
-  return docs;
+
+  return docs
+    .map((item) => {
+      item.objectID = item._id;
+      if (item._extension?.endsWith("yml")) {
+        item.previews = item.previews.map((img: string) => {
+          return `https://imagedelivery.net/C1c8i0JtRURCOUA0iRLBpQ/wcao${item._path?.replace(
+            /\//g,
+            "-"
+          )}-${img.split(".")[0].replace("/", "")}/sm`;
+        });
+      }
+      return item;
+    })
+    .filter((item) => item._extension?.endsWith("yml"));
 });
