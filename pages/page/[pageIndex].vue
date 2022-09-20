15c70939258d7157352fe9757e91258566bf760d
xiaoqiyang <meetqy@qq.com>
2022-09-20 10:09:41 +0800

    chore: tags

--- pages/page/[pageIndex].vue
+++ pages/page/[pageIndex].vue
@@ -18,7 +18,7 @@
           <img
             v-lazy="useCFContentVLazy(item._path, item.previews[0], 'sm')"
             class="bg-base-200/20 border border-base-300 min-h-[300px] object-contain object-center rounded-box cursor-zoom-in"
-            :alt="`${title} template in ${tags.join(
+            :alt="`${item.title} template in ${item.tags.join(
               '/'
             )}, based on tailwindcss,daisyui. index › sm`"
           />
