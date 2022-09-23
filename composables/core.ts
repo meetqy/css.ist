917a1549b6c5f9de073f6bb20ef03de50e76a249
xiaoqiyang <meetqy@qq.com>
2022-09-23 11:05:48 +0800

    chore: 一次事故

--- composables/core.ts
+++ composables/core.ts
@@ -15,7 +15,7 @@ export const useCF = (
   type: "public" | "sm" | "2xl" | string,
   format?: "webp" | "avif" | "json"
 ): string => {
-  const baseURL = "https://img.css.ist";
+  const baseURL = "https://imagedelivery.net/C1c8i0JtRURCOUA0iRLBpQ";
   return `${baseURL}/${id}/${type}?format=${format || "webp"}`;
 };
 
