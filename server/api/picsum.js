bde1cb2ca418f25f962c81719160294c1981c30e
xiaoqiyang <meetqy@qq.com>
2022-09-29 11:16:06 +0800

    chore: test picsum

---
+++ server/api/picsum.js
@@ -0,0 +1,11 @@
+export default defineEventHandler(async (event) => {
+  const query = useQuery(event);
+
+  const response = await $fetch.raw(query.url, {
+    responseType: "arrayBuffer",
+  });
+
+  const contentType = response.headers.get("Content-Type");
+
+  return send(event, Buffer.from(response._data), contentType);
+});
--- server/api/picsum.ts
+++
@@ -1,5 +0,0 @@
-export default defineEventHandler(async (event) => {
-  const img = await $fetch("https://picsum.photos/200/300");
-
-  return img;
-});
