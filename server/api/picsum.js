d6ab3e7263848079ade1fc0853d88c4d4bd3d999
xiaoqiyang <meetqy@qq.com>
2022-09-29 11:58:31 +0800

    chore: test

--- server/api/picsum.js
+++ server/api/picsum.js
@@ -1,11 +1,11 @@
 export default defineEventHandler(async (event) => {
   const query = useQuery(event);
 
-  const response = await $fetch.raw(query.url, {
+  const response = await $fetch.raw(query.url + `?random=${Math.random()}`, {
     responseType: "arrayBuffer",
   });
 
   const contentType = response.headers.get("Content-Type");
 
-  send(event, Buffer.from(response._data), contentType);
+  return send(event, Buffer.from(response._data), contentType);
 });
