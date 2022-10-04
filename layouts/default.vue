aab4e3370f1433e0c9b29cc9498c74e5e52a6715
xiaoqiyang <meetqy@qq.com>
2022-10-04 17:49:47 +0800

    chore: 测试优化

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,7 @@
 
       <aside
         ref="drawerSide"
-        class="w-80 h-screen font-serif capitalize bg-base-200/50 pb-24"
+        class="w-80 h-screen overflow-y-auto font-serif capitalize lg:bg-base-200/50 bg-base-100/80 backdrop-blur pb-24"
       >
         <ul class="menu rounded-box p-2 w-full">
           <li class="lg:hidden">
@@ -59,7 +59,7 @@ import SimpleBar from "simplebar";
 import "simplebar/dist/simplebar.css";
 
 const drawerSide = ref(null);
-watch(drawerSide, (e) => new SimpleBar(e));
+watch(drawerSide, (e) => !isMobile() && new SimpleBar(e));
 
 const res = await queryContent("introduce").only(["tags"]).find();
 
